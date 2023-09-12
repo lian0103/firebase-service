130c06f8027e108db8998cb90279848dea840a20
Jason Lien 連宗聖 <jasonlien@starlux-airlines.com>
2023-09-12 15:49:07 +0800

    feat: FCM test

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPpXwlEC2gxidiytOdHOnjCMeet0kjukI",
@@ -9,9 +10,27 @@ const firebaseConfig = {
   appId: "1:742672094917:web:b2dc641497e89aff1c82ac",
 };
 
+function requestPermission(message) {
+  console.log("Requesting permission...");
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
+      return true;
+    }
+  });
+}
+
 // Initialize Firebase
-export const initFirebase = () => {
-  initializeApp(firebaseConfig);
+export const initFirebase = async () => {
+  const app = initializeApp(firebaseConfig);
+  const message = getMessaging(app);
+
+  await requestPermission(message);
+
+  getToken(message, {
+    vapidKey:
+      "BMvqTbOYx0np9__r5J0Dlg7WlXDcZ6QnkWfh1H2_91AT1MRDwRnsUcChpbGDfKLb0SAj35zwr7Zd5BDrJigjZvY",
+  });
 };
 
 export default {};
